e33bc49bebe69d7a84cd2dfc3adda5dd8345227b
synzeri <cjramirez105@gmail.com>
2026-02-11 02:17:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function selectOption(option) {
 
 // Function to flash rainbow colors and then execute a callback function
 function flashRainbowColors(callback) {
-    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+    var colors = ['#066cbe', '#0e4691', '#0a7ecc', '#ee6cd2', '#cf5bac', '#cf46f1', '#e669d5'];
     var i = 0;
     var interval = setInterval(function() {
         document.body.style.backgroundColor = colors[i];
@@ -58,19 +58,27 @@ function displayCat() {
 
 // Function to display the cat-heart.gif
 function displayCatHeart() {
-    // Clear existing content in the image container
-    document.getElementById('image-container').innerHTML = '';
-    // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
+    imageContainer.innerHTML = '';
+
     // Create a new Image element for the cat-heart
     var catHeartImage = new Image();
-    // Set the source (file path) for the cat-heart image
-    catHeartImage.src = 'cathappyy.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
-    // Set alternative text for the image (for accessibility)
+    catHeartImage.src = 'cathappyy.gif';
     catHeartImage.alt = 'Cat Heart';
-    // When the cat-heart image is fully loaded, add it to the image container
+
+    // Create text element
+    var text = document.createElement('p');
+    text.innerText = 'Yayyy! gdgeriufesigesngdrnhriufnesuifesnfesunvdriugnbdrnhhsefesnesiugbesugesgfiesnsevndsriuvesgsebguesiessgdruisnviesnvesguldsgdshgnshesnhesngesilfnseiuveshgeshesh 💖💖💖💖💖';
+    text.style.fontSize = '24px';
+    text.style.marginTop = '15px';
+    text.style.color = '#cf46f1';
+    text.style.fontWeight = 'bold';
+
+    // When image loads, add image and text
     catHeartImage.onload = function() {
         imageContainer.appendChild(catHeartImage);
+        imageContainer.appendChild(text);
+
         // Hide the options container
         document.getElementById('options').style.display = 'none';
     };
